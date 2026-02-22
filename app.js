21df466baf6271b406116b2fca26a3e849dece92
Tharindu Sasmitha <sasa@192.168.1.4>
2026-02-23 01:27:19 +0530

    Added use module feature

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-const https = require('https');
-https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
-let data = '';
-resp.on('data', (chunk) => {
-data += chunk;
-});
-resp.on('end', () => {
-console.log(JSON.parse(data));
-});
-});
+const myModule = require('./my-module.js');
+console.log(myModule.myFunction());
